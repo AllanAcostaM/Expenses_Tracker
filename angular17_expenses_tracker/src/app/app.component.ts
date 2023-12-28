91ac2b8e889046639b9c2744aee97ef9ab8fc7c4
Joseph Acosta <allanacost.27@gmail.com>
2023-12-28 00:31:05 -0500

    Angular: Componente Transactions y @for para recorrer una lista

--- angular17_expenses_tracker/src/app/app.component.ts
+++ angular17_expenses_tracker/src/app/app.component.ts
@@ -3,14 +3,18 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 
+// Models
+import { Balance } from './models/balance.model';
+import { Transaction } from './models/transaction.model';
+
 // Components
 import { BalanceComponent } from './components/balance/balance.component';
-import { Balance } from './models/balance.model';
+import { TransactionsComponent } from './components/transactions/transactions.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, BalanceComponent],
+  imports: [CommonModule, RouterOutlet, BalanceComponent, TransactionsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -20,4 +24,34 @@ export class AppComponent {
     income: 100_000,
     expenses: 45_000,
   };
+  transactions: Transaction[] = [
+    {
+      id: '1',
+      type: 'expense',
+      amount: 45,
+      category: 'food',
+      date: new Date(2023, 12, 24),
+    },
+    {
+      id: '2',
+      type: 'expense',
+      amount: 280,
+      category: 'shopping',
+      date: new Date(2023, 12, 20),
+    },
+    {
+      id: '3',
+      type: 'expense',
+      amount: 60,
+      category: 'entertainment',
+      date: new Date(2023, 12, 15),
+    },
+    {
+      id: '4',
+      type: 'income',
+      amount: 500,
+      category: 'payrrol',
+      date: new Date(2023, 12, 2),
+    },
+  ]
 }
