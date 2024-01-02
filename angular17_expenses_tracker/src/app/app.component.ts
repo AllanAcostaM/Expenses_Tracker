8bc42cba59437850fa1177e2dc1ac56f5d949b7b
Joseph Acosta <allanacost.27@gmail.com>
2024-01-01 19:34:41 -0500

    Angular: Refactor utilizando los componentes Home, AddTransaction y About

--- angular17_expenses_tracker/src/app/app.component.ts
+++ angular17_expenses_tracker/src/app/app.component.ts
@@ -3,63 +3,14 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 
-// Models
-import { Balance } from './models/balance.model';
-import { Transaction } from './models/transaction.model';
-
-// Components
-import { BalanceComponent } from './components/balance/balance.component';
-import { TransactionsComponent } from './components/transactions/transactions.component';
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, BalanceComponent, TransactionsComponent, RouterLink],
+  imports: [CommonModule, RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 
 export class AppComponent {
-  balance: Balance = {
-    amount: 55_000,
-    income: 100_000,
-    expenses: 45_000,
-  };
-  transactions: Transaction[] = [
-    {
-      id: '1',
-      type: 'expense',
-      amount: 45,
-      category: 'food',
-      date: new Date(2023, 11, 24),
-    },
-    {
-      id: '2',
-      type: 'expense',
-      amount: 280,
-      category: 'shopping',
-      date: new Date(2023, 11, 20),
-    },
-    {
-      id: '3',
-      type: 'expense',
-      amount: 60,
-      category: 'entertainment',
-      date: new Date(2023, 11, 15),
-    },
-    {
-      id: '4',
-      type: 'income',
-      amount: 500,
-      category: 'payroll',
-      date: new Date(2023, 11, 2),
-    },
-  ]
 
-  removeTransaction(id: string) {
-    // Elimina la transaccion del arreglo
-    this.transactions = this.transactions.filter(
-      transaction => transaction.id !== id
-    );
-  }
 }
