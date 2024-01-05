951f8f7b3f57e292ccc835bb7419a782703fcae2
Joseph Acosta <allanacost.27@gmail.com>
2024-01-05 08:29:44 -0500

    Angular: Recalcula los saldos del componente Balance

--- angular17_expenses_tracker/src/app/pages/home/home.component.ts
+++ angular17_expenses_tracker/src/app/pages/home/home.component.ts
@@ -23,10 +23,11 @@ import { TransactionsService } from '../../services/transactions.service';
 })
 export class HomeComponent implements OnInit {
   transactions!: Transaction[]
+
   balance: Balance = {
-    amount: 55_000,
-    income: 100_000,
-    expenses: 45_000,
+    amount: 0,
+    income: 0,
+    expenses: 0,
   };
 
   // Provee el servicio al componente usando Denpendency Injection
@@ -35,11 +36,12 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.transactionService.get().subscribe((response: Transaction[]) => {
       this.transactions = response
-    })
+      // Recalcula los saldos
+      this.calculateBalance();
+    });
   }
 
   removeTransaction(id: string) {
-
     // Elimina la trnasaccion del backend
     this.transactionService.remove(id).subscribe((response: Transaction) => {
       console.log(response);
@@ -47,9 +49,24 @@ export class HomeComponent implements OnInit {
       this.transactions = this.transactions.filter(
         transaction => transaction.id !== id
       );
+      // Recalcula los saldos
+      this.calculateBalance();
     })
+  }
 
 
-
+  calculateBalance(): void {
+    let income: number = 0;
+    let expenses: number = 0;
+    // Recorre las transacciones para calcular los saldos
+    this.transactions.forEach(transaction => {
+      if (transaction.type === "expense")
+        expenses += transaction.amount;
+      if (transaction.type === "income") income += transaction.amount;
+    });
+    // Calcula el saldo total
+    const amount: number = income - expenses;
+    // Actualiza los saldos
+    this.balance = { amount, income, expenses, }
   }
 }
