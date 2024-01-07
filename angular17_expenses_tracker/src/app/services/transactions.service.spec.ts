3416d337204b832fc77970527320b3162f0424fc
Joseph Acosta <allanacost.27@gmail.com>
2024-01-07 18:44:25 -0500

    Angular: Pruebas de Service

--- angular17_expenses_tracker/src/app/services/transactions.service.spec.ts
+++ angular17_expenses_tracker/src/app/services/transactions.service.spec.ts
@@ -1,16 +1,44 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
+// Sevices
 import { TransactionsService } from './transactions.service';
+
+// Modules
+import { Transaction } from '../models/transaction.model';
 
 describe('TransactionsService', () => {
   let service: TransactionsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule]
+    });
     service = TestBed.inject(TransactionsService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+  it('should be 3 transactions', (done) => {
+    service.get().subscribe((transactions: Transaction[]) => {
+      expect(transactions.length).toBe(3)
+      done();
+    });
+  });
+  it('should get the right categories', (done) => {
+    service.get().subscribe((transactions: Transaction[]) => {
+
+      const incomes: Transaction[] = transactions.filter(transaction => transaction.type === 'income'
+      );
+      // Verifica la cantidad de incomes
+      expect(incomes.length).toBe(1);
+      // Verifica la categoria de income resultante
+      expect(incomes[0].category).toBe('payroll');
+      // Verifica las categorias obtenidas
+      expect(transactions[1].category).toBe('food');
+      expect(transactions[2].category).toBe('entertaiment')
+      done();
+    })
+  })
 });
